f7010d5ed9f0f22bf5699a0dd9cadf0534292ed1
muradguluzadecodes <quluzade.m.98@gmail.com>
2024-01-15 19:43:04 +0400

    Added Piechart about durations

--- src/features/dashboard/DurationChart.jsx
+++ src/features/dashboard/DurationChart.jsx
@@ -1,4 +1,17 @@
+/* eslint-disable no-unused-vars */
+
+import PropTypes from "prop-types";
 import styled from "styled-components";
+import Heading from "../../ui/Heading";
+import {
+  Cell,
+  Legend,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+} from "recharts";
+import { useDarkMode } from "../../context/DarkModeContext";
 
 const ChartBox = styled.div`
   /* Box */
@@ -64,37 +77,37 @@ const startDataLight = [
 const startDataDark = [
   {
     duration: "1 night",
-    value: 0,
+    value: 4,
     color: "#b91c1c",
   },
   {
     duration: "2 nights",
-    value: 0,
+    value: 2,
     color: "#c2410c",
   },
   {
     duration: "3 nights",
-    value: 0,
+    value: 7,
     color: "#a16207",
   },
   {
     duration: "4-5 nights",
-    value: 0,
+    value: 5,
     color: "#4d7c0f",
   },
   {
     duration: "6-7 nights",
-    value: 0,
+    value: 6,
     color: "#15803d",
   },
   {
     duration: "8-14 nights",
-    value: 0,
+    value: 2,
     color: "#0f766e",
   },
   {
     duration: "15-21 nights",
-    value: 0,
+    value: 4,
     color: "#1d4ed8",
   },
   {
@@ -130,3 +143,52 @@ function prepareData(startData, stays) {
 
   return data;
 }
+
+// eslint-disable-next-line react/prop-types
+function DurationChart({ confirmedStays }) {
+  const { isDarkMode } = useDarkMode();
+  const startData = isDarkMode ? startDataDark : startDataLight;
+  const data = prepareData(startData, confirmedStays);
+
+  console.log(confirmedStays);
+  return (
+    <ChartBox>
+      <Heading as="h2">Stay duration summary</Heading>
+      <ResponsiveContainer width="100%" height={240}>
+        <PieChart>
+          <Pie
+            data={data}
+            nameKey="duration"
+            dataKey="value"
+            width={800}
+            height={400}
+            innerRadius={85}
+            outerRadius={110}
+            cx="40%"
+            cy="50%"
+            paddingAngle={3}
+          >
+            {data.map((entry) => (
+              <Cell
+                fill={entry.color}
+                stroke={entry.color}
+                key={entry.duration}
+              />
+            ))}
+          </Pie>
+          <Tooltip />
+          <Legend
+            verticalAlign="middle"
+            align="right"
+            width="30%"
+            layout="vertical"
+            iconSize={15}
+            iconType="circle"
+          />
+        </PieChart>
+      </ResponsiveContainer>
+    </ChartBox>
+  );
+}
+
+export default DurationChart;
